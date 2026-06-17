21ef27d4dd88fda5b19a653600dc12a74332dca0
Alan <alanwnuczko@protonmail.com>
2026-06-17 20:29:10 +0200

    Liczby pierwsze

---
+++ Algorytmy/czy_pierwsza.py
@@ -0,0 +1,11 @@
+def czy_pierwsza(n):
+    if n < 2:
+        return False
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+print(czy_pierwsza(37))
